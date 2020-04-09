88066da09d13274aeb6678fafa6f33961bc2ce75
kathyqat <k_qin@hotmail.com>
2020-04-08 22:18:15 -0400

    Completed button to reset and change grid size

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 let container = document.querySelector('.container');
-let i=0;
+let numOfSquares = 16;
 
-do{
-  let div = document.createElement('div');
-  container.appendChild(div);
-  i++;
-} while(i<(16*16));
+function createGrid(numOfSquares){
+  for (let i=0; i<(numOfSquares*numOfSquares); i++){
+    let div = document.createElement('div');
+    div.setAttribute('class', 'square');
+    container.appendChild(div);
+  };
+};
 
-let squares = document.querySelectorAll('div');
+createGrid(numOfSquares);
 
-squares.forEach((div) => {
-  div.addEventListener('mouseover', (e) => {
-    div.classList.add('hover');
+function hoverSquares(){
+  let squares = document.querySelectorAll('.square');
+  
+  squares.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+      div.classList.add('hover');
+    });
   });
+};
+
+hoverSquares();
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', (e) => {
+  let removedSquares = document.querySelectorAll('.square');
+  
+  removedSquares.forEach((div) => {
+    container.removeChild(div);
+  });
+  
+  numOfSquares = window.prompt("How many squares should the sides of the new grid be?");
+  
+  createGrid(numOfSquares);
+  container.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`;
+  hoverSquares();
 });
