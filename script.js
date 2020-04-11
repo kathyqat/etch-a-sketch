d2f93463607c9f4d245fbac0bc33b1b32653ce18
kathyqat <k_qin@hotmail.com>
2020-04-11 16:20:07 -0400

    Change square to random colour and shade on repeat mouse overs

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
 let numOfSquares = 16;
 
+//don't put formulas in for loop initialization, make const
 function createGrid(numOfSquares){
-  for (let i=0; i<(numOfSquares*numOfSquares); i++){
+  let gridSize = numOfSquares*numOfSquares;
+
+  for (let i=0; i<gridSize; i++){
     let div = document.createElement('div');
     div.setAttribute('class', 'square');
+    div.setAttribute('data-count', '0');
     container.appendChild(div);
   };
 };
@@ -14,16 +18,38 @@ createGrid(numOfSquares);
 function hoverSquares(){
   let squares = document.querySelectorAll('.square');
   
+  //can replace div with square, it's just a parameter name
   squares.forEach((div) => {
     div.addEventListener('mouseover', (e) => {
-      div.classList.add('hover');
+      // div.classList.add('hover');
+      let divColor = div.style.backgroundColor;
+      
+      if (divColor == ""){
+        let randomColor = randomNum();
+        div.style.backgroundColor = `hsl(${randomColor}, 100%, 50%)`;
+      } else {
+        let hoverCount = trackCount(div);
+        let lightness = (1-hoverCount*0.1);
+        div.style.filter = `brightness(${lightness})`;
+      };
     });
   });
 };
 
+function randomNum(){
+  return Math.floor(Math.random() * 360);
+};
+
+function trackCount(div){
+  let count = div.getAttribute('data-count');
+  count++;
+  div.setAttribute('data-count', `${count}`);
+  return count;
+};
+
 hoverSquares();
 
-let button = document.querySelector('button');
+const button = document.querySelector('button');
 
 button.addEventListener('click', (e) => {
   let removedSquares = document.querySelectorAll('.square');
